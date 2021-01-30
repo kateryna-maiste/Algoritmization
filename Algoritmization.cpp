548440580464153e8b765a769105416a5a2fd925
Катерина Майстер <kateryna.maister@nure.ua>
2021-01-31 00:51:40 +0200

    Добавьте файлы проекта.

---
+++ Algoritmization.cpp
@@ -0,0 +1,45 @@
+﻿// Algoritmization.cpp 
+
+//Обчислити вираз y=2a-(x+6)
+
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+
+using namespace std;
+
+int main()
+
+{
+	setlocale(LC_ALL, "rus");
+
+	
+	int x;
+	int y;
+	int a;
+	int b;
+	int mult;
+	int sum;
+
+	cout << "y = 2 * a - (b + 6)" << endl;
+	cout << "Введите значение: a= " << endl;
+	cin >> a;
+	cout << "Введите значение: x= " << endl;
+	cin >> b;
+	
+	
+	mult = 2 * a;
+
+	sum = b + 6;
+	
+	cout << "y =" << mult - sum; 
+
+	cout << "\n Верно!" << endl ;
+
+
+
+
+
+}
+
